1d357bec2f94623ba018f91ac35099c14846c5be
Irene Bajan <irnesh@gmail.com>
2016-12-03 20:14:50 -0500

    First commit

---
+++ EmojiApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EmojiApp
+//
+//  Created by Irene Bajan on 2016-12-03.
+//  Copyright © 2016 Irene Bajan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
